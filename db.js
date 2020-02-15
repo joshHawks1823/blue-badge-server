1b01b4d46890381962aaaeb049637c1563dd9759
joshhawks1823 <jheat23@gmail.com>
2020-02-15 11:14:47 -0500

    second commit

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("inventory-database", "postgres", "password", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres"
 });
-
 sequelize.authenticate().then(
   function() {
     console.log("Connected to inventory-database postgres database");
